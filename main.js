62208fe9f54fcc76f44aabf5003b09d2a1ac55ee
Donut5896 <Nutchanokchon5@gmail.com>
2021-01-17 23:55:52 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const card = document.querySelectorAll(".card");
+
+
+card.forEach( item => {
+    item.addEventListener('mouseover', (event) => {
+    //event.target.style.backgroundColor = "blue";
+})
+     
+})
